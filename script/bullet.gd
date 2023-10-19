7fb21baedd1bfaabb3039c3a2e972edb149d86fe
orkeilius <orkeilius@users.noreply.github.com>
2023-10-19 17:01:24 +0200

    add bullet collide with each other

--- script/bullet.gd
+++ script/bullet.gd
@@ -29,14 +29,19 @@ func _on_area_2d_area_entered(target : Area2D):
 		if damage == 0:
 			queue_free()
 	
+	elif target.is_in_group("bullet") and origin != target.origin:
+		speed = 0
+		_AnimatedSprite2D.sprite_frames = load("res://sprite/bullet/bulletExplosion.tres")
+		await get_tree().create_timer(0.25).timeout
+		queue_free()
+		
 	elif target.name == "gameBorder":
 		queue_free()
 
 	elif target.is_in_group("player") and origin != "player":
 		queue_free()
-
+	
 func _on_body_entered(body):
-
 	if body.is_in_group("wall"):
 		body.collide(global_position,true)
 		queue_free()
