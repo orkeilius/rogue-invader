120e8207ea062e46c860328bbb45a8a2b8df23a6
orkeilius <orkeilius@users.noreply.github.com>
2023-10-19 13:47:39 +0200

    add player stunt when hit

--- script/gamemode/gamemodeClassic.gd
+++ script/gamemode/gamemodeClassic.gd
@@ -2,6 +2,7 @@ extends Node
 
 var effects = [ShootUp.new(),MoveNormal.new()] 
 var hp = 3
+var score = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
