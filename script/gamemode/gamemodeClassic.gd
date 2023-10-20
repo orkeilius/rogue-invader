2c0ea33e18dc22283d2d652e9404974e4ed4152c
orkeilius <orkeilius@users.noreply.github.com>
2023-10-20 11:19:44 +0200

    add autoload

--- script/gamemode/gamemodeClassic.gd
+++ script/gamemode/gamemodeClassic.gd
@@ -1,8 +1,9 @@
 extends Node
 
+@onready var globalData = get_node("/root/GameData")
+
 var effects = [ShootUp.new(),MoveNormal.new()] 
 var hp = 3: set = _set_hp
-
 var _score = [0,0]
 var score :int : set = _set_score, get = _get_score
 var player = 0
@@ -13,6 +14,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	_set_score(globalData.score)
 	pass
 
 
@@ -23,8 +25,14 @@ func _set_hp(new_value: int) -> void:
 
 func _set_score(new_value : int) -> void:
 	_score[player] = new_value
+	globalData.score = new_value
 	find_child("Score1").text = "%05d" % _score[0]
 	find_child("Score2").text = "%05d" % _score[1]
 
 func _get_score() -> int:
 	return _score[player]
+
+func nextLevel():
+	globalData.level += 1
+	get_tree().change_scene_to_file("res://scene/gameMode/gamemodeClassic.tscn")
+
