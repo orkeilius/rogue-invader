eccef73afd5275d5fc09ad14e644e40ab84f3388
orkeilius <orkeilius@users.noreply.github.com>
2023-11-17 15:35:57 +0100

    fix more pierce effect

--- script/effect/userEffect/MorePierce.gd
+++ script/effect/userEffect/MorePierce.gd
@@ -4,7 +4,7 @@ class_name MorePierce extends AbstractEffect
 const name = "more pierce"
 
 func applyBallEffect(bullet:Bullet) -> Bullet:
-	bullet.pierce += level
+	bullet.pierce += level * 2
 	# add effect to bullet before shoot
 	return bullet
 
